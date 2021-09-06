17675a4853a520ee75eaec4b0184cf027354316d
gmanmar <gabriel.manzur@simplit-solutions.com>
2021-09-05 23:09:32 -0300

    Add express and debug tools

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('tiny'));
+
+app.get('/', (req, res) => {
+    res.send('gorank');
+});
+
+app.listen(3000, () => {
+    debug(`Listening on port  ${chalk.green('3000')}`);
+});
+
